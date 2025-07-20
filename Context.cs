cb6330b2d4219630105263bf0dee82bb25994971
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-20 13:04:13 +0300

    Add files via upload

---
+++ Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_OOP.Entity;
+
+namespace Project_OOP.Data
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=GUNESBERKANT\\SQLEXPRESS;database=DbNewOopCore;integrated security=true;encrypt=false;");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
