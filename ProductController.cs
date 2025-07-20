c48a27e5c8229aee11b0f1b4809fa7562564c139
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-20 13:04:02 +0300

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_OOP.Data;
+
+namespace Project_OOP.Controllers
+{
+    public class ProductController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var values = context.Products.ToList(); // context üzerinden products icinde yer alan degerleri listeledik. ToList metotu ile.
+            return View(values); // atadıgımız  degiskeni burada döndürdük ki bunları view tarafında karşılayabilmek için.
+        }
+    }
+}
